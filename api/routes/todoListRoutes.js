2515830e3fed1000420fc5e4e06a97e56f95248e
Olatunde Garuba <generalgmt@yahoo.co.uk>
2017-03-03 21:20:38 +0100

    routes pluralized

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -4,11 +4,11 @@ module.exports = function(app) {
 	var todoList = require('../controllers/todoListController');
 
 	// todoList Routes
-	app.route('/task')
+	app.route('/tasks')
 		.get(todoList.list_all_tasks)
 		.post(todoList.create_a_task);
 
-	app.route('/task/:taskId')
+	app.route('/tasks/:taskId')
 		.get(todoList.read_a_task)
 		.put(todoList.update_a_task)
 		.delete(todoList.delete_a_task);
