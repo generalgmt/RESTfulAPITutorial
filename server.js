3e9971810d23ba906a481aba5d558000b4bb3c45
Wesley Ramalho <wesley_raa@hotmail.com>
2019-10-24 18:26:25 -0300

    changing default port number

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 3060,
   mongoose = require('mongoose'),
   Task = require('./api/models/todoListModel'),
   bodyParser = require('body-parser');
