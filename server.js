a73ac778701b27ea4d0939646c81450a43d1b497
Wesley Ramalho <wesley_raa@hotmail.com>
2019-10-24 20:15:31 -0300

    fixing connection with mongodb

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-var express = require('express'),
+var express = require("express"),
   app = express(),
-  port = process.env.PORT || 3060,
-  mongoose = require('mongoose'),
-  Task = require('./api/models/todoListModel'),
-  bodyParser = require('body-parser');
+  port = 3060,
+  mongoose = require("mongoose"),
+  Task = require("./api/models/todoListModel"),
+  bodyParser = require("body-parser");
 
 mongoose.Promise = global.Promise;
-mongoose.createConnection('mongodb://localhost/Tododb');
-
+mongoose.connect("mongodb://localhost/Tododb");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-var routes = require('./api/routes/todoListRoutes');
+var routes = require("./api/routes/todoListRoutes");
 routes(app);
 
 app.use(function(req, res) {
-  res.status(404).send({url: req.originalUrl + ' not found'})
+  res.status(404).send({ url: req.originalUrl + " not found" });
 });
 
 app.listen(port);
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log("todo list RESTful API server started on: " + port);
